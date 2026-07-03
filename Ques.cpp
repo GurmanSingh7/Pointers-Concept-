14c8b1044316047532efb5dd3a6f41205900cbdf
Gurman Singh <gurmansingh57801@gmail.com>
2026-07-03 10:35:32 +0530

    Update Ques.cpp

--- Ques.cpp
+++ Ques.cpp
@@ -1,28 +1,34 @@
 //Q - Write a program to count how many times the function is called ?   
-// #include<iostream>
-// using namespace std;
-// static int count = 0;    
-// void fun()  
-// {
-//     count++;
-//     cout<<"Function called : "<<count<<"times"<<endl;
-// }
-// int main(){
-//     fun();
-//     fun();
-//     fun();
-//     return 0;
-// }
 
-//another
 #include<iostream>
 using namespace std;
+
+static int count = 0;    
+void fun()  
+{
+    count++;
+    cout<<"Function called : "<<count<<"times"<<endl;
+}
+
+int main(){
+    fun();
+    fun();
+    fun();
+    return 0;
+}
+
+// Another Code : 
+
+#include<iostream>
+using namespace std;
+
 int* fun()
 {
     static int i = 0;
     i++;
     return &i;
 }
+
 int main()
 {
     fun();
